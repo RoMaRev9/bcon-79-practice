db740ed91a41ca4057b0cfe676af42c6f83d0a41
RomanREV <github2025rev@gmail.com>
2026-01-27 16:30:06 +0200

    зміни 6 папка

--- module-06/script.js
+++ module-06/script.js
@@ -1,9 +1,24 @@
 //  Task 1
-
+// const goods = new Goods("apple", 23.5);
+// console.log(goods);
 //  Task 2
-
+// const goods = new Goods(
+//   "apple",
+//   23.5,
+//   "https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg",
+//   400
+// );
+// console.log(goods);
 //  Task 3
-
+const goods = new Goods(
+  "apple",
+  23.5,
+  "https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg",
+  400
+);
+console.log(goods);
+// І тепер виведемо на сторінку
+document.querySelector(".out-3").append(goods.draw());
 //  Task 4
 
 //  Task 5

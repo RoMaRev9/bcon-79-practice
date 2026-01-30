36e2514e2c338a5e7c0f26584ce36ad7eec55edc
RomanREV <github2025rev@gmail.com>
2026-01-30 17:03:27 +0200

    add initial implementation of tasks in script.js

---
+++ module-06/script.js
@@ -0,0 +1,54 @@
+//  Task 1
+// const goods = new Goods("apple", 23.5);
+// console.log(goods);
+//  Task 2
+// const goods = new Goods(
+//   "apple",
+//   23.5,
+//   "https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg",
+//   400
+// );
+// console.log(goods);
+//  Task 3
+// const goods = new Goods(
+//   "apple",
+//   23.5,
+//   "https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg",
+//   400
+// );
+// console.log(goods);
+// // І тепер виведемо на сторінку
+// document.querySelector(".out-3").append(goods.draw());
+// //  Task 4
+
+// //  Task 5
+// const goods5 = new InheriteGoods(
+//   "apple",
+//   23.5,
+//   "https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg",
+//   400,
+//   true
+// );
+// console.log(goods5);
+
+//  Task 2
+
+//  Task 3
+
+//  Task 4
+
+//  Task 5
+
+//  Task 6
+
+//  Task 7
+
+//  Task 8
+
+//  Task 9
+
+//  Task 10
+
+//  Task 11
+
+//  Task 12
